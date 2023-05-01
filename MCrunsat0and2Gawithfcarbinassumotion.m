30fcb84eda2b0636845843a567ea7f0973b123ec
shileigeo <71109611+shileigeo@users.noreply.github.com>
2023-05-01 21:13:20 +0800

    Add files via upload

---
+++ MCrunsat0and2Gawithfcarbinassumotion.m
@@ -0,0 +1,109 @@
+clear all;
+load('RMgPh.mat');
+L=100000000;
+ageinter=0:0.01:2000;
+RMginter1=interp1(AgePh,RMgPh,ageinter,'linear');
+%% 
+for i=1:200001
+  
+    decay=i:1:200001;
+    Rdecay=exp(-0.001.*(decay-i)/100);
+    swpreinter(i)=sum(RMginter1(i:end).*Rdecay)/sum(Rdecay);
+end
+RMg2Ga=RMginter1(end);
+%% modern data
+
+for i=1:20
+fcarbin=normrnd(0.42,0.04,[1 L]);
+indexss=find(fcarbin>0&fcarbin<1);
+fcarbin=fcarbin(indexss);
+fcarbout=rand(1,length(fcarbin));
+carbin=swpreinter(1)+normrnd(-1.5,0.2,[1 length(fcarbin)]);
+silin= normrnd(-0.4,0.1,[1 length(fcarbin)]);
+frsil=normrnd(0.8,0.4,[1 length(fcarbin)]);
+frcarb=normrnd(-1.5,0.2,[1 length(fcarbin)]);
+misfit=fcarbin.*(carbin-silin)-fcarbout.*(frcarb-frsil)-frsil+0.83+silin;
+Df=abs(misfit);
+sindex=find(Df<0.001);
+fcarbinsolved=fcarbin(sindex);fcarboutsolved=fcarbout(sindex);
+
+if i==1
+fcarbinsolveds(i,:)=fcarbinsolved(1:50000);
+fcarboutsolveds(i,:)=fcarboutsolved(1:50000);
+
+else 
+fcarbinsolveds=[fcarbinsolveds fcarbinsolved(1:50000)];
+fcarboutsolveds=[fcarboutsolveds fcarboutsolved(1:50000)];
+
+end
+i
+end
+mfcarbinsolved=mean(fcarbinsolveds);
+mfcarboutsolved=mean(fcarboutsolveds);
+
+%% 2Ga
+% fcarbin=[0 1]; fcarbout=[0 1]; carbin=[-2.1 -1.5]; frsil=[0 0.8];
+for i=1:20
+fcarbin=normrnd(0.14,0.04,[1 L]);
+indexss=find(fcarbin>0&fcarbin<1);
+fcarbin=fcarbin(indexss);
+fcarbout=rand(1,length(fcarbin));
+carbin=swpreinter(length(swpreinter))+normrnd(-1.5,0.2,[1 length(fcarbin)]);
+silin= normrnd(-0.4,0.1,[1 length(fcarbin)]);
+frsil=normrnd(0.8,0.4,[1 length(fcarbin)]);
+frcarb=normrnd(-1.5,0.2,[1 length(fcarbin)]);
+misfit1=fcarbin.*(carbin-silin)-fcarbout.*(frcarb-frsil)-frsil-RMg2Ga+silin;
+Df1=abs(misfit1);
+sindex1=find(Df1<0.001);
+fcarbinsolved1=fcarbin(sindex1);fcarboutsolved1=fcarbout(sindex1);
+if i==1
+fcarbinsolveds1(i,:)=fcarbinsolved1(1:50000);
+fcarboutsolveds1(i,:)=fcarboutsolved1(1:50000);
+
+else 
+fcarbinsolveds1=[fcarbinsolveds1 fcarbinsolved1(1:50000)];
+fcarboutsolveds1=[fcarboutsolveds1 fcarboutsolved1(1:50000)];
+
+
+end 
+j=i
+end
+
+mfcarbinsolved1=mean(fcarbinsolveds1);
+mfcarboutsolved1=mean(fcarboutsolveds1);
+fcarbinsolvedall=[fcarbinsolveds fcarbinsolveds1];
+fcarboutsolvedall=[fcarboutsolveds fcarboutsolveds1];
+
+% plot(fcarbinsolved,fcarboutsolved,'o','Markersize',1);
+
+%%
+X=fcarbinsolvedall;
+Y=fcarboutsolvedall;
+Xmin=min(X);Xmax=max(X);
+Ymin=min(Y);Ymax=max(Y);
+
+Nx=200;
+Ny=200;
+
+Xedge=linspace(Xmin,Xmax,Nx);
+Yedge=linspace(Ymin,Ymax,Ny);
+
+
+[N,~,~,binX,binY] = histcounts2(X,Y,[-inf,Xedge(2:end-1),inf],[-inf,Yedge(2:end-1),inf]);
+
+XedgeM=movsum(Xedge,2)/2;
+YedgeM=movsum(Yedge,2)/2;
+
+[Xedgemesh,Yedgemesh]=meshgrid(XedgeM(2:end),YedgeM(2:end));
+
+%%
+
+fcarbinsolveds2=fcarbinsolveds(1:100000);fcarboutsolveds2=fcarboutsolveds(1:100000);
+fcarbinsolveds3=fcarbinsolveds1(1:100000);fcarboutsolveds3=fcarboutsolveds1(1:100000);
+scatter(fcarbinsolveds2,fcarboutsolveds2,0.1,'MarkerFaceColor','r','MarkerEdgeColor','r',...
+    'MarkerFaceAlpha',.1,'MarkerEdgeAlpha',.1);
+hold on;
+scatter(fcarbinsolveds3,fcarboutsolveds3,0.1,'MarkerFaceColor','b','MarkerEdgeColor','b',...
+    'MarkerFaceAlpha',.1,'MarkerEdgeAlpha',.1);
+xlabel('fcarbin');ylabel('fcarbout');
+
---
+++ NDBwithoutfcarbinassumption.m
@@ -0,0 +1,99 @@
+clear all;
+load('RMgPh.mat');
+ageinter1=0:0.01:2000;
+RMginter2=interp1(AgePh,RMgPh,ageinter1,'linear');
+for i=1:200001
+   
+    decay=i:1:200001;
+    Rdecay=exp(-0.001.*(decay-i)/100);
+    swpre(i)=sum(RMginter2(i:end).*Rdecay)/sum(Rdecay);
+end
+ageinter2=0:0.001:2000;
+swpreinter=interp1(ageinter1,swpre,ageinter2,'linear');
+RMginter=interp1(AgePh,RMgPh,ageinter2,'linear');
+%% pick random timespot for calculation
+for i=1:100
+tt=rand(1,500000000)*2000;
+tt=fix(tt*1000);ttindex=tt+1;
+%% definition of paramaters
+
+fcarbin=rand(1, length(tt));
+fcarbout=rand(1,length(fcarbin));
+ carbin=swpreinter(ttindex)+normrnd(-1.5,0.2,[1 length(fcarbin)]);silin= normrnd(-0.4,0.1,[1 length(fcarbin)]);
+frsil=normrnd(0.8,0.4,[1 length(fcarbin)]);
+frcarb=normrnd(-1.5,0.2,[1 length(fcarbin)]);
+RMgsolv=RMginter(ttindex);
+sw=RMgsolv+normrnd(0,0.1,[1 length(fcarbin)]);
+misfit=fcarbin.*(carbin-silin)-fcarbout.*(frcarb-frsil)-frsil-sw+silin;
+Df=abs(misfit);
+sindex=find(Df<0.001);
+fcarbinsolved=fcarbin(sindex);fcarboutsolved=fcarbout(sindex);
+carbinsolved=carbin(sindex);frsilsolved=frsil(sindex);
+ttsolved=ttindex(sindex)-1;swsolved=sw(sindex);
+frcarbsolved=frcarb(sindex);silinsolved=silin(sindex);
+R=(fcarboutsolved-fcarbinsolved);
+R1=(1-fcarbinsolved)./(1-fcarboutsolved);
+if i==1
+Rsolveds(i,:)=R(1:250000);
+ttsolveds(i,:)=ttsolved(1:250000);
+R1solveds(i,:)=R1(1:250000);
+fcarbinsolveds(i,:)=fcarbinsolved(1:250000);
+fcarboutsolveds(i,:)=fcarboutsolved(1:250000);
+else 
+Rsolveds=[Rsolveds R(1:250000)];
+ttsolveds=[ttsolveds ttsolved(1:250000)];
+R1solveds=[R1solveds R1(1:250000)];
+fcarbinsolveds=[fcarbinsolveds fcarbinsolved(1:250000)];
+fcarboutsolveds=[fcarboutsolveds fcarboutsolved(1:250000)];
+end 
+i
+end
+
+%%
+
+Y=Rsolveds;
+X=ttsolveds/1000;
+
+Xmin=min(X);Xmax=max(X);
+Ymin=min(Y);Ymax=max(Y);
+
+Nx=300;
+Ny=300;
+
+
+Xedge=linspace(Xmin,Xmax,Nx);
+Yedge=linspace(Ymin,Ymax,Ny);
+
+
+[N,~,~,binX,binY] = histcounts2(X,Y,[-inf,Xedge(2:end-1),inf],[-inf,Yedge(2:end-1),inf]);
+
+XedgeM=movsum(Xedge,2)/2;
+YedgeM=movsum(Yedge,2)/2;
+
+[Xedgemesh,Yedgemesh]=meshgrid(XedgeM(2:end),YedgeM(2:end));
+
+
+XX=ttsolveds/1000;YY=Rsolveds;
+XX=round(XX);
+
+j=1;
+for ii=1:2001
+   mRsort(j)=mean(YY(find(XX==(ii-1))));
+  eRsort(j)=std(YY(find(XX==(ii-1))));
+   Agesort(j)=(ii-1);
+    j=j+1;
+end
+%%
+mmRsort(1)= mRsort(1);Agesort2(1)=0;
+eeRsort(1)=eRsort(1);
+for iii=1:200
+    mmRsort(iii+1)=mRsort(iii*10+1);
+    eeRsort(iii+1)=eRsort(iii*10+1);
+   Agesort2(iii+1)=iii*10;
+end
+%% 
+
+figure(1)
+pcolor(Xedgemesh,Yedgemesh,N'); hold on;
+plot(Agesort2,mmRsort);hold on;plot(Agesort2,mmRsort-eeRsort);plot(Agesort2,mmRsort+eeRsort);hold off;
+ylim([-0.3 0.6]);
